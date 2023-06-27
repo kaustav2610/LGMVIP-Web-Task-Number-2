e834fdb71cb20346bb7f2808bdb61e9dde0a6894
kaustav2610 <kastav2610@gmail.com>
2023-06-27 17:41:27 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState} from 'react';
+import './index.css';
 
-function App() {
+const App = () => {
+  const [users, setUsers] = useState([]);
+
+  const loadUsers = async () => {
+    const response = await fetch('https://reqres.in/api/users?page=1');
+    const jsonResponse = await response.json();
+    setUsers(jsonResponse.data); 
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <nav className="navbar">
+        <span className="brand">Myntra Users : </span>
+        <button className="get-users-btn" onClick={loadUsers}>
+          Get Users
+        </button>
+      </nav>
+
+      <div className="user-grid">
+        {users.map(({ id, avatar, last_name, email }) => (
+          <div className="user-card" key={id}>
+            <img src={avatar} alt={last_name} className="user-avatar" />
+            <h3>{`${last_name}`}</h3>
+            <p>{email}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
